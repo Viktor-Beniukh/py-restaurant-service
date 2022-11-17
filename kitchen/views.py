3897b65f4a7656e7d52eb128181ef7c156e943a4
Viktor Beniukh <superbigbenjamin@gmail.com>
2022-11-17 16:18:30 +0200

    Create home page on the server

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen.models import Cook, DishType, Dish, Ingredient
+
+
+def index(request):
+    num_cooks = Cook.objects.count()
+    num_dish_types = DishType.objects.count()
+    num_dishes = Dish.objects.count()
+    num_ingredients = Ingredient.objects.count()
+
+    context = {
+        "num_cooks": num_cooks,
+        "num_dish_types": num_dish_types,
+        "num_dishes": num_dishes,
+        "num_ingredients": num_ingredients,
+    }
+
+    return render(request, "kitchen/index.html", context=context)
